e94c25aa665abcfcd87bc1fe30c838233d5822fd
Elvira <elvirarosa304@gmail.com>
2026-01-27 21:32:39 +0700

    firrst commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:admin_sistem_akademik/widget/custom_sidebar.dart';
+import 'package:flutter/material.dart';
+
+import 'screen/dashboard_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Admin Sistem Akademik',
+      home: const MainNavigation()
+      // home: const DashboardPage(),
+      
+    );
+  }
+}
+
+
---
+++ lib/screen/dashboard_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import 'package:admin_sistem_akademik/theme/design_system.dart';
+import 'package:admin_sistem_akademik/widget/custom_sidebar.dart';
+
+class DashboardPage extends StatelessWidget {
+  const DashboardPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const  Text("Dashboard Page");
+  }
+}
